61a91c17969176b64dd7a5909d14ebc4b3119980
Nightara <webmin@nightara.de>
2021-07-05 13:44:55 +0200

    Added Mock for SSH connection

--- airflow/executors/remote_lsf_executor.py
+++ airflow/executors/remote_lsf_executor.py
@@ -1,7 +1,8 @@
 import os
+import random
 import re
 import time
-from typing import Optional, Any
+from typing import Optional, Any, Dict, Tuple, Iterable
 from urllib.parse import urlparse
 
 from airflow.configuration import conf, log
@@ -11,6 +12,16 @@ from paramiko.rsakey import RSAKey
 from paramiko import SSHClient
 
 from airflow.utils.state import State
+
+
+class LsfTask:
+
+    def __init__(self, lsf_id: str, lsf_name: str, running: bool, finished: bool, failed: bool):
+        self.lsf_id = lsf_id
+        self.lsf_name = lsf_name
+        self.running = running
+        self.finished = finished
+        self.failed = failed
 
 
 class RemoteLsfExecutor(BaseExecutor):
@@ -35,7 +46,6 @@ class RemoteLsfExecutor(BaseExecutor):
         self.task_to_bjob = dict()
         self.runnings_tasks = dict()
 
-        # TODO: Mock SSH connection
         # Test SSH connection
         ssh_client = SSHClient()
         ssh_client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
@@ -148,7 +158,7 @@ class RemoteLsfExecutor(BaseExecutor):
                     self.fail(self.bjob_to_task[bjob.lsf_id])
 
     @staticmethod
-    def _parse_bjob(line: str):
+    def _parse_bjob(line: str) -> LsfTask:
         data = line.split()
         return LsfTask(data[0], data[6], data[2] == "RUN", data[2] == "DONE", data[2] == "EXIT")
 
@@ -172,11 +182,76 @@ class SshConnector:
         self.client.close()
 
 
-class LsfTask:
+class MockClient(SSHClient):
 
-    def __init__(self, lsf_id: str, lsf_name: str, running: bool, finished: bool, failed: bool):
-        self.lsf_id = lsf_id
-        self.lsf_name = lsf_name
-        self.running = running
-        self.finished = finished
-        self.failed = failed
+    def __init__(self):
+        self.ids = dict()
+        self.ids[0] = 0
+        self.status_message = {
+            0: "PEND",
+            1: "RUN",
+            2: "DONE",
+            3: "EXIT",
+        }
+
+    def exec_command(
+        self,
+        command: str,
+        bufsize: int = ...,
+        timeout: Optional[float] = ...,
+        get_pty: bool = ...,
+        environment: Optional[Dict[str, str]] = ...,
+    ) -> Tuple[None, Iterable, Iterable]:
+        if "bsub" in command:
+            queue = re.match("-q (\\w+) ", command)
+            if queue:
+                new_id = 0
+                while new_id in self.ids:
+                    new_id = random.randint(0, 10000)
+                self.ids[new_id] = 1
+                return None, ["Job <%d> is submitted to queue <%s>.\n" % (new_id, queue.group(1))], []
+            return None, [], ["Invalid input"]
+
+        elif "bjobs" in command:
+            data = list()
+            for job_id, job_status in self.ids:
+                if job_status < 4:
+                    data.append("%d a %s c d e Test_Name" % (job_id, self.status_message[job_status]))
+                    self.ids[job_id] = max(self.ids[job_id] + 1, 2)
+            return None, data, []
+
+        elif "bkill" in command:
+            job_id = re.match("bkill (\\d+) ", command)
+            if job_id and int(job_id.group(1)) in self.ids:
+                self.ids[int(job_id.group(1))] = 3
+            return None, [], ["Invalid job id"]
+
+        raise ValueError("Invalid SSH command " + command)
+
+
+class MockConnector:
+
+    def __init__(self):
+        self.client = MockClient()
+        self.exception = None
+
+    def __enter__(self) -> MockClient:
+        self.exception = None
+        return self.client
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.exception = (exception_type, exception_value, exception_traceback)
+
+
+class MockLsfExecutor(RemoteLsfExecutor):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def start(self):
+        """No config needed."""
+        self.bjob_to_task = dict()
+        self.task_to_bjob = dict()
+        self.runnings_tasks = dict()
+
+        self.connector = MockConnector()
